d6fa94c803298d1a1a9f56ee28a1768da247a4df
Alex Costa <alexmcosta.dev@gmail.com>
2015-09-25 11:50:13 -0700

    Add comments to app.js

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ signIn.config(function($stateProvider, $urlRouterProvider) {
     templateUrl: "partials/home.html"
   });
 
+  //Make a state that will display a list of students
+  //and allow students to sign in.
   $stateProvider.state('', {
     url:"/students",
     templateUrl:"partials/signIn.html",
     controller: 'StudentsCtrl'
   });
 
+  //Make a state for the instructor to see who is here.
   $stateProvider.state('', {
     url: "/teacher",
     templateUrl: "partials/whoIsHere.html",
