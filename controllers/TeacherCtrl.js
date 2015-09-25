78b5bdf165a34822a0df9d3b68bbc1dfba438f7e
Alex Costa <alexmcosta.dev@gmail.com>
2015-09-25 13:04:06 -0700

    Fix typo in TeacherCtrl

--- controllers/TeacherCtrl.js
+++ controllers/TeacherCtrl.js
@@ -1,4 +1,4 @@
-attendance.controller('TeachersCtrl', function TeachersCtrl($scope, StudentsFactory) {
+attendance.controller('TeacherCtrl', function TeacherCtrl($scope, StudentsFactory) {
     //Just pulls the data from the factory to then list out on the 'Who is here' page.
     $scope.students = StudentsFactory.students;
     $scope.StudentsFactory = StudentsFactory;
